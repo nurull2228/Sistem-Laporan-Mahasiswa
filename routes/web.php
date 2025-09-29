19f8c3463283b8e66c34a8566f7186a12d878b4b
Ni'ma Nurul Kholifah <nurullkhlfh2207@gmail.com>
2025-09-29 10:26:12 +0700

    Membuat migrasi

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Sistem Pelaporan Masalah - Teknik Informatika";
+});
+
+Route::get('/daftar', function () {
+    return "Daftar masalah";
+});
+
+Route::get('/daftar-masalah', function () {
+    return "List Daftar Masalah";
 });
